9f7362b5b0bc20d90ea65a3be962d6ccfc89ebbd
Sofian Slimani <s.slimani@gmx.de>
2017-11-08 18:42:02 +0100

    Detect Height

--- ReIdentificator/BodyDetector.cs
+++ ReIdentificator/BodyDetector.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.Kinect;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ReIdentificator
 {
@@ -7,9 +9,10 @@ namespace ReIdentificator
     {
         private BodyFrameReader bodyFrameReader = null;
         private Body[] bodies = null;
-
+        private List<BodyDetector_body> bodiesToProcess = new List<BodyDetector_body>();
         private KinectSensor kinect;
         private Comparer comparer;
+        public int minimumDetectionPerBody = 3;
         public BodyDetector(KinectSensor kinect, Comparer comparer)
         {
             this.kinect = kinect;
@@ -21,7 +24,6 @@ namespace ReIdentificator
         private void Reader_BodyFrameArrived(object sender, BodyFrameArrivedEventArgs e)
         {
             bool dataReceived = false;
-            bool hasTrackedBody = false;
             using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
             {
                 if (bodyFrame != null)
@@ -33,24 +35,71 @@ namespace ReIdentificator
 
             if (dataReceived)
             {
-
                 // iterate through each body
                 for (int bodyIndex = 0; bodyIndex < this.bodies.Length; bodyIndex++)
                 {
                     Body body = this.bodies[bodyIndex];
                     if (body.IsTracked)
                     {
-                        
-                        Debug.WriteLine("body in view with id: " + body.TrackingId);                       
-                        hasTrackedBody = true;
-                    }                  
-                }
+                        if (!bodiesToProcess.Exists(element => element.TrackingId == body.TrackingId))
+                        {
+                            bodiesToProcess.Add(new BodyDetector_body(body.TrackingId));
+                        }
+                        BodyDetector_body _body = bodiesToProcess.Find(element => element.TrackingId == body.TrackingId);
+                        if (body.Height(3) > 0)
+                            _body.heights.Add(body.Height(3));
+                    }
 
-                if (!hasTrackedBody)
+                }
+                // detect if body has left frame, then process it
+                for (int i = 0; i < this.bodiesToProcess.Count; i++)
                 {
-                    // Do something when no bodies are there
+                    BodyDetector_body b = bodiesToProcess[i];
+                    bool visibleInFrame = false;
+                    for (int bodyIndex = 0; bodyIndex < this.bodies.Length; bodyIndex++)
+                    {
+                        Body body = this.bodies[bodyIndex];
+                        if (body.TrackingId == b.TrackingId)
+                            visibleInFrame = true;
+
+                    }
+                    // body left frame
+                    if (!visibleInFrame) {
+                        bodiesToProcess.RemoveAt(i);
+                        processBody(b);
+                    }
                 }
             }
         }
+        private void processBody(BodyDetector_body _body)
+        {
+            //TODO: ignore extremes?
+            if (_body.heights.Count >= minimumDetectionPerBody)
+            {
+                _body.height = _body.heights.Average();
+            }
+            else
+            {
+                _body.height = -1;
+            }
+
+        }
+    }
+    class BodyDetector_body
+    {
+        public ulong TrackingId { get; set; }
+        public double height { get; set; } // -1 if not detected properly
+        public List<double> heights { get; set; } = new List<double>();
+
+        //TODO
+        public double torsoheight { get; set; }
+        public List<double> torsoHeights { get; set; } = new List<double>();
+        
+        public BodyDetector_body(ulong trackingId)
+        {
+            this.TrackingId = trackingId;
+        }
+
+
     }
 }
---
+++ ReIdentificator/BodyHeightHelper.cs
@@ -0,0 +1,112 @@
+﻿// CREDITS
+// partly based on https://pterneas.com/2012/06/08/kinect-for-windows-find-user-height-accurately/
+// and https://github.com/jhealy/kinect2
+//
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associateddocumentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+// THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+
+using System;
+using Microsoft.Kinect;
+
+public static class BodyHeightExtension
+{
+
+    public static double Height(this Body TargetBody, int minLegJoints)
+    {
+        if (TargetBody == null) return -1.0;
+        if (TargetBody.IsTracked == false) return -2.0;
+
+        const double HEAD_DIVERGENCE = 0.1;
+
+        Joint _head = TargetBody.Joints[JointType.Head];
+        Joint _neck = TargetBody.Joints[JointType.Neck];
+
+        Joint _spine = TargetBody.Joints[JointType.SpineShoulder];
+        Joint _waist = TargetBody.Joints[JointType.SpineBase];
+        Joint _hipLeft = TargetBody.Joints[JointType.HipLeft];
+        Joint _hipRight = TargetBody.Joints[JointType.HipRight];
+        Joint _kneeLeft = TargetBody.Joints[JointType.KneeLeft];
+        Joint _kneeRight = TargetBody.Joints[JointType.KneeRight];
+        Joint _ankleLeft = TargetBody.Joints[JointType.AnkleLeft];
+        Joint _ankleRight = TargetBody.Joints[JointType.AnkleRight];
+        Joint _footLeft = TargetBody.Joints[JointType.FootLeft];
+        Joint _footRight = TargetBody.Joints[JointType.FootRight];
+
+        // Find which leg is tracked more accurately.
+        int legLeftTrackedJoints = NumberOfTrackedJoints(_hipLeft, _kneeLeft, _ankleLeft, _footLeft);
+        int legRightTrackedJoints = NumberOfTrackedJoints(_hipRight, _kneeRight, _ankleRight, _footRight);
+
+        if (legLeftTrackedJoints < minLegJoints && legRightTrackedJoints < minLegJoints)
+        {
+            return -3;
+        }
+
+
+        double legLength = legLeftTrackedJoints > legRightTrackedJoints ? Length(_hipLeft, _kneeLeft, _ankleLeft, _footLeft)
+            : Length(_hipRight, _kneeRight, _ankleRight, _footRight);
+        double _retval = Length(_head, _neck, _spine, _waist) + legLength + HEAD_DIVERGENCE;
+
+        return _retval;
+    }
+
+    /// <summary>
+    /// Returns the upper height of the specified skeleton (head to waist).
+    /// </summary>
+    public static double UpperHeight(this Body TargetBody)
+    {
+        Joint _head = TargetBody.Joints[JointType.Head];        
+        Joint _neck = TargetBody.Joints[JointType.SpineMid];        
+        Joint _spine = TargetBody.Joints[JointType.SpineShoulder];        
+        Joint _waist = TargetBody.Joints[JointType.SpineBase];
+        return Length(_head, _neck, _spine, _waist);
+    }
+
+    /// <summary>
+    /// Returns the length of the segment defined by the specified joints.
+    /// </summary>
+    /// <param name="p1">The first joint (start of the segment).</param>
+    /// <param name="p2">The second joint (end of the segment).</param>
+    /// <returns>The length of the segment in meters.</returns>
+    public static double Length(Joint p1, Joint p2)
+    {
+        return Math.Sqrt(
+            Math.Pow(p1.Position.X - p2.Position.X, 2) +
+            Math.Pow(p1.Position.Y - p2.Position.Y, 2) +
+            Math.Pow(p1.Position.Z - p2.Position.Z, 2));
+    }
+
+    /// <summary>
+    /// Returns the length of the segments defined by the specified joints.
+    /// </summary>
+    /// <param name="joints">A collection of two or more joints.</param>
+    /// <returns>The length of all the segments in meters.</returns>
+    public static double Length(params Joint[] joints)
+    {
+        double length = 0;
+
+        for (int index = 0; index < joints.Length - 1; index++)
+        {
+            length += Length(joints[index], joints[index + 1]);
+        }
+
+        return length;
+    }
+
+    /// <summary>
+    /// Given a collection of joints, calculates the number of the joints that are tracked accurately.
+    /// </summary>
+    public static int NumberOfTrackedJoints(params Joint[] joints)
+    {
+        int trackedJoints = 0;
+        foreach (var joint in joints)
+        {
+            if (joint.TrackingState == TrackingState.Tracked)
+            {
+                trackedJoints++;
+            }
+        }
+        return trackedJoints;
+    }
+}
