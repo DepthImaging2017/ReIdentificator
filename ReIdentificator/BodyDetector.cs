659508e6b5909a13da555c54f6d4561877713671
Sofian Slimani <s.slimani@gmx.de>
2017-11-15 19:18:14 +0100

    [Body] print additional parameters

--- ReIdentificator/BodyDetector.cs
+++ ReIdentificator/BodyDetector.cs
@@ -117,7 +117,7 @@ namespace ReIdentificator
                 valueToAdd = body.DistanceBetweenTwoJoints(JointType.SpineMid, JointType.ShoulderLeft);
                 if (valueToAdd > 0)
                     _body.spineMidToLeftShoulder_list.Add(valueToAdd);
-                valueToAdd = body.DistanceBetweenTwoJoints(JointType.SpineMid, JointType.ShoulderLeft);
+                valueToAdd = body.DistanceBetweenTwoJoints(JointType.SpineMid, JointType.ShoulderRight);
                 if (valueToAdd > 0)
                     _body.spineMidToRightShoulder_list.Add(valueToAdd);
             }
@@ -141,7 +141,7 @@ namespace ReIdentificator
                 _body.rightHipToSpineBase = Util.trimmedMean(_body.rightHipToSpineBase_list, trimmedMeanPercentage);
                 _body.spineMidToLeftShoulder = Util.trimmedMean(_body.spineMidToLeftShoulder_list, trimmedMeanPercentage);
                 _body.spineMidToRightShoulder = Util.trimmedMean(_body.spineMidToRightShoulder_list, trimmedMeanPercentage);
-                UI.printLog("body parameters: " + _body.neckToSpineMid + " " + _body.spineMidToSpineBase + " " + _body.neckToLeftShoulder + " " + _body.leftHipToSpineBase + " " + _body.spineMidToLeftShoulder);
+                UI.printLog("body parameters: " + _body.height + " - " +  _body.neckToSpineMid + " - " + _body.spineMidToSpineBase + " - " + _body.neckToLeftShoulder + " - " + _body.neckToRightShoulder + " - " + _body.leftHipToSpineBase + " - " + _body.rightHipToSpineBase + " - " + _body.spineMidToLeftShoulder + " - " + _body.spineMidToRightShoulder);
             }
         }
     }
