4a481e77fab23e608a8983bfcab8d3f11cdc64bd
Christoph Friedrich <christoph@friedrich-whv.de>
2017-12-05 19:21:02 +0100

    add color extraction to ImageProcessor
    Uses the BodyProcessor's `bodies` property to reduce duplicate code and
    calculations

--- ReIdentificator/ImageProcessor.cs
+++ ReIdentificator/ImageProcessor.cs
@@ -19,6 +19,8 @@ namespace ReIdentificator
         private byte[] colorPixels;
         private WriteableBitmap colorBitmap;
         private MainWindow mainWindow;
+        private Dictionary<ulong, List<byte[]>> colors = new Dictionary<ulong, List<byte[]>>();
+
         public ImageProcessor(KinectSensor kinect, Comparer comparer, MainWindow mainWindow)
         {
             this.kinect = kinect;
@@ -27,6 +29,8 @@ namespace ReIdentificator
 
             this.colorFrameReader = this.kinect.ColorFrameSource.OpenReader();
             this.colorFrameReader.FrameArrived += this.Reader_ColorFrameArrived;
+
+            mainWindow.BodyLeftView += HandleBodyLeftViewEvent;
         }
 
         private void Reader_ColorFrameArrived(object sender, ColorFrameArrivedEventArgs e)
@@ -48,8 +52,61 @@ namespace ReIdentificator
 
                     //mainWindow.FrameDisplayImage.Source = this.colorBitmap;
 
+                    Body[] bodies = this.mainWindow.getBodyProcessor().getBodies();
+                    if (bodies != null)
+                    {
+                        for (int bodyIndex = 0; bodyIndex < bodies.Length; bodyIndex++)
+                        {
+                            if(bodies[bodyIndex].IsTracked) {
+                                Joint shoulderLeft = bodies[bodyIndex].Joints[JointType.ShoulderLeft];
+                                ColorSpacePoint colorPoint = kinect.CoordinateMapper.MapCameraPointToColorSpace(shoulderLeft.Position);
+                                Debug.WriteLine("screen coords: " + colorPoint.X + " , " + colorPoint.Y);
+
+                                // calculate pixel index of joint coordinate
+                                int pixelIndex = (int)Math.Floor(colorPoint.Y);
+                                pixelIndex *= 1920;
+                                pixelIndex += (int)Math.Floor(colorPoint.X);
+                                pixelIndex *= 4;
+                                
+                                // extract color components
+                                byte red = colorPixels[pixelIndex + 2];
+                                byte green = colorPixels[pixelIndex + 1];
+                                byte blue = colorPixels[pixelIndex];
+                                byte opacity = colorPixels[pixelIndex + 3];
+                                
+                                // log
+                                Debug.WriteLine("color: " + red + ", " + green + ", " + blue + ", " + opacity);
+                                
+                                // output to user
+                                mainWindow.ColorBox.Background = new SolidColorBrush(Color.FromRgb(red, green, blue));
+
+                                // save in this body's color timeseries
+                                byte[] asarray = { red, green, blue, opacity };
+                                if (!this.colors.ContainsKey(bodies[bodyIndex].TrackingId))
+                                {
+                                    // if necessary instantiate list
+                                    this.colors[bodies[bodyIndex].TrackingId] = new List<byte[]>();
+                                }
+                                // save it
+                                this.colors[bodies[bodyIndex].TrackingId].Add(asarray);
+                            }
+                        }
+                    }
                 }
             }
+        }
+
+        string averageColor(List<byte[]> colors)
+        {
+            // for now, this is just a mockup
+            byte[] avg = colors.First<byte[]>();
+            return avg[0] + "," + avg[1] + "," + avg[2] + ',' + avg[3];
+        }
+
+        void HandleBodyLeftViewEvent(object sender, LeftViewEventArgs e)
+        {
+            Console.WriteLine("body with id " + e.TrackingId + " has left frame");
+            mainWindow.printLog("average color of left shoulder of person with id " + e.TrackingId + ": " + averageColor(this.colors[e.TrackingId]));
         }
     }
 }
